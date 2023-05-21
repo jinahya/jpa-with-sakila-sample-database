06b30a21b76316c49dec0a0e784fa21f548264d8
Jin Kwon <onacit@gmail.com>
2023-05-21 18:44:08 +0900

    wip

---
+++ doc/sakila-db.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+tar xzf ./sakila-db.tar.gz
+cp ./sakila-db/sakila-schema.sql ../.docker-entrypoint-initdb.d/01.schema.sql
+cp ./sakila-db/sakila-data.sql ../.docker-entrypoint-initdb.d/03.data.sql
